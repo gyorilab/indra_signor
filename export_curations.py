70c60f15b92b1a8192fe1ecaba295fa7cdeb497a
Ben Gyori <ben.gyori@gmail.com>
2024-02-01 13:43:21 -0500

    Implement row merge logic

--- export_curations.py
+++ export_curations.py
@@ -78,6 +78,49 @@ def curations_to_rows(curations):
         ]
 
 
+def merge_curation_rows(curation_rows):
+    # Find rows without sites and find rows with sites
+    # Merge sentences of rows without sites into rows with sites
+    row_dicts = [dict(zip(header, row)) for row in curation_rows]
+    rows_no_site = [row for row in row_dicts if not row['RESIDUE']]
+    rows_with_site = [row for row in row_dicts if row['RESIDUE']]
+    # We have to have both to do merging
+    if rows_with_site and rows_no_site:
+        new_rows = []
+        for row in rows_no_site:
+            for row_with_site in rows_with_site:
+                if row['EFFECT'] == row_with_site['EFFECT']:
+                    row_wit_site_sentences = set(row_with_site['SENTENCE'].split('|'))
+                    row_sentences = set(row['SENTENCE'].split('|'))
+                    unique_sentences = '|'.join(list(row_sentences |
+                                                     row_wit_site_sentences))
+                    new_row = row_with_site.copy()
+                    new_row['SENTENCE'] = unique_sentences
+
+                    new_rows.append(new_row)
+    else:
+        new_rows = row_dicts
+    # Then look at the remaining rows and merge any that are exactly the same
+    # except for the sentences, and merge the set of unique sentences into
+    # one consolidated row
+    rows_by_key = defaultdict(list)
+    for row in new_rows:
+        row_key = (row['EFFECT'], row['MECHANISM'], row['RESIDUE'])
+        rows_by_key[row_key].append(row)
+    merged_rows = []
+    for key, rows in rows_by_key.items():
+        if len(rows) == 1:
+            merged_rows.append(rows[0].values())
+        else:
+            sentences = set()
+            for row in rows:
+                sentences |= set(row['SENTENCE'].split('|'))
+            new_row = rows[0].copy()
+            new_row['SENTENCE'] = '|'.join(list(sentences))
+            merged_rows.append(new_row.values())
+    return merged_rows
+
+
 def get_pair_key(stmt, ev):
     subj = stmt.subj if isinstance(stmt, RegulateActivity) else stmt.enz
     obj = stmt.obj if isinstance(stmt, RegulateActivity) else stmt.sub
@@ -120,7 +163,9 @@ if __name__ == '__main__':
         print(key)
         for s, ev, cur in curations:
             print(s, ev.text)
-        all_rows += curations_to_rows(curations)
+        curation_rows = list(curations_to_rows(curations))
+        curation_rows = merge_curation_rows(curation_rows)
+        all_rows += curation_rows
 
     with open('dephosphorylations_with_reg_export.csv', 'wt') as fh:
         writer = csv.writer(fh, delimiter=',', quotechar='"')
